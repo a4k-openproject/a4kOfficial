0501efca179171957a3e33e260f9d85e45c7d963
drinfernoo <drinfernoo@yahoo.com>
2022-06-10 08:48:35 -0700

    :sparkles: - Add Amazon Prime provider

---
+++ providers/a4kOfficial/en/adaptive/amazonprime.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+from future.standard_library import install_aliases
+
+install_aliases()
+
+from providerModules.a4kOfficial.core import Core
+
+
+class sources(Core):
+    def __init__(self):
+        super(sources, self).__init__()
+        self._providers = ['amp', 'amz']
+        self._scraper = "amazonprime"
+        self._service = "amazonprime"
+        self._movie_url = "plugin://plugin.video.amazon-test/?asin={}&mode=PlayVideo&name=None&adult=0&trailer=0&selbitrate=0"
+        self._episode_url = "plugin://plugin.video.amazon-test/?asin={}&mode=PlayVideo&name=None&adult=0&trailer=0&selbitrate=0"
+
+    def _get_service_id(self, item):
+        offers = item["offers"]
+        service_offers = [
+            o for o in offers if o['package_short_name'] in self._providers
+        ]
+        if not service_offers:
+            return None
+
+        offer = service_offers[0]
+        url = offer['urls'][self._scheme]
+        id = url.rstrip('/').split('gti=')[1]
+
+        return id
