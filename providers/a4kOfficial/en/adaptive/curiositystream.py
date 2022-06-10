74873ee9e7b9cb6c2e4b1426eb8339f0c45891a1
drinfernoo <drinfernoo@yahoo.com>
2022-06-10 14:24:06 -0700

    :sparkles: - Add Curiosity Stream provider

---
+++ providers/a4kOfficial/en/adaptive/curiositystream.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+from future.standard_library import install_aliases
+
+install_aliases()
+
+from providerModules.a4kOfficial.core import Core
+
+
+class sources(Core):
+    def __init__(self):
+        super(sources, self).__init__()
+        self._providers = ['cts']
+        self._scheme = "standard_web"
+        self._movie_url = "plugin://{}/?_=play&_play=1&id={}"
+        self._episode_url = "plugin://{}/?_=play&_play=1&id={}"
