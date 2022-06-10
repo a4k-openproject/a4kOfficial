b533080d9f11a6adfb91790b972b1f69a508256d
drinfernoo <drinfernoo@yahoo.com>
2022-06-10 14:24:16 -0700

    :sparkles: - Add HBO Max provider

---
+++ providers/a4kOfficial/en/adaptive/hbomax.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+from future.standard_library import install_aliases
+
+install_aliases()
+
+from providerModules.a4kOfficial.core import Core
+
+
+class sources(Core):
+    def __init__(self):
+        super(sources, self).__init__()
+        self._providers = ['hmf', 'hbm', 'hbo', 'hbn']
+        self._scheme = "standard_web"
+        self._movie_url = "plugin://{}/?_=play&slug={}"
+        self._episode_url = "plugin://{}/?_=play&slug={}"
