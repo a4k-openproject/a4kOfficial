6c56cafac1df9aa079a4576cc6f6cb05621d1111
drinfernoo <drinfernoo@yahoo.com>
2022-07-29 13:43:18 -0700

    :construction: - Add encoding flags and imports

--- providerModules/a4kOfficial/core.py
+++ providerModules/a4kOfficial/core.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+from future.standard_library import install_aliases
+
+install_aliases()
+
 import time
 
 import xbmcgui
--- providers/a4kOfficial/configure.py
+++ providers/a4kOfficial/configure.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, unicode_literals
+from future.standard_library import install_aliases
+
+install_aliases()
 
 import xbmcaddon
 
